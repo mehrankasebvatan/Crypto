8ad5d6509caa6760e4b9b15e16f2752c9363565e
MehranKasebvatan <mehran.kasebvatan@gmail.com>
2026-05-28 14:57:07 +0330

    refactor(setup): apply Clean Architecture folder structure

--- app/src/main/java/ir/kasebvatan/crypto/MainActivity.kt
+++ app/src/main/java/ir/kasebvatan/crypto/MainActivity.kt
@@ -11,7 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import ir.kasebvatan.crypto.ui.theme.CryptoTheme
+import ir.kasebvatan.crypto.presentation.ui.theme.CryptoTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
--- app/src/main/java/ir/kasebvatan/crypto/presentation/ui/theme/Type.kt
+++ app/src/main/java/ir/kasebvatan/crypto/presentation/ui/theme/Type.kt
@@ -1,4 +1,4 @@
-package ir.kasebvatan.crypto.ui.theme
+package ir.kasebvatan.crypto.presentation.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
