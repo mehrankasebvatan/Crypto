474d4962d72c991e68d516f857a4b83656ce959f
MehranKasebvatan <mehran.kasebvatan@gmail.com>
2026-05-29 17:47:08 +0330

    feat(domain): add getCoinsPaged to CoinRepository

--- app/src/main/java/ir/kasebvatan/crypto/data/repository/CoinRepositoryImpl.kt
+++ app/src/main/java/ir/kasebvatan/crypto/data/repository/CoinRepositoryImpl.kt
@@ -1,7 +1,11 @@
 package ir.kasebvatan.crypto.data.repository
 
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.paging.PagingData
 import ir.kasebvatan.crypto.data.local.dao.CoinDao
 import ir.kasebvatan.crypto.data.remote.CoinGeckoApiService
+import ir.kasebvatan.crypto.data.remote.CoinPagingSource
 import ir.kasebvatan.crypto.domain.model.Coin
 import ir.kasebvatan.crypto.domain.model.CoinDetail
 import ir.kasebvatan.crypto.domain.repository.CoinRepository
@@ -35,5 +39,15 @@ class CoinRepositoryImpl @Inject constructor(
     override fun getCoinDetail(id: String): Flow<CoinDetail> = flow {
         val coin = api.getCoinDetail(id).toCoinDetail()
         emit(coin)
+    }
+
+    override fun getCoinsPaged(): Flow<PagingData<Coin>> {
+        return Pager(
+            config = PagingConfig(
+                pageSize = 20,
+                prefetchDistance = 5
+            ),
+            pagingSourceFactory = { CoinPagingSource(api) }
+        ).flow
     }
 }
--- app/src/main/java/ir/kasebvatan/crypto/domain/repository/CoinRepository.kt
+++ app/src/main/java/ir/kasebvatan/crypto/domain/repository/CoinRepository.kt
@@ -1,13 +1,12 @@
 package ir.kasebvatan.crypto.domain.repository
 
+import androidx.paging.PagingData
 import ir.kasebvatan.crypto.domain.model.Coin
 import ir.kasebvatan.crypto.domain.model.CoinDetail
 import kotlinx.coroutines.flow.Flow
 
 interface CoinRepository {
-
     fun getCoins(): Flow<List<Coin>>
-
     fun getCoinDetail(id: String): Flow<CoinDetail>
-    
+    fun getCoinsPaged(): Flow<PagingData<Coin>>
 }
