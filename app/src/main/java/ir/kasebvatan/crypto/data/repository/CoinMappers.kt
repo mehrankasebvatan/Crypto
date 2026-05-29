5b0cc92a72999b9ec4c85a6171e93645a59b3896
MehranKasebvatan <mehran.kasebvatan@gmail.com>
2026-05-29 10:55:35 +0330

    feat(data): add CoinDto and CoinDetail mappers to domain models

---
+++ app/src/main/java/ir/kasebvatan/crypto/data/repository/CoinMappers.kt
@@ -0,0 +1,33 @@
+package ir.kasebvatan.crypto.data.repository
+
+import ir.kasebvatan.crypto.data.remote.dto.CoinDetailDto
+import ir.kasebvatan.crypto.data.remote.dto.CoinDto
+import ir.kasebvatan.crypto.domain.model.Coin
+import ir.kasebvatan.crypto.domain.model.CoinDetail
+
+fun CoinDto.toCoin(): Coin {
+    return Coin(
+        id = id,
+        symbol = symbol,
+        name = name,
+        image = image,
+        currentPrice = currentPrice,
+        priceChangePercentage24h = priceChangePercentage24h,
+        marketCap = marketCap,
+        marketCapRank = marketCapRank,
+    )
+}
+
+fun CoinDetailDto.toCoinDetail(): CoinDetail {
+    return CoinDetail(
+        id = id,
+        symbol = symbol,
+        name = name,
+        description = description.en,
+        image = image.large,
+        marketCapRank = marketCapRank,
+        currentPrice = marketData.currentPrice["usd"] ?: 0.0,
+        priceChangePercentage24h = marketData.priceChangePercentage24h,
+        marketCap = marketData.marketCap["usd"] ?: 0L,
+    )
+}
