24bccf682d02dbca52ac6cc0f641d625284d8d02
MehranKasebvatan <mehran.kasebvatan@gmail.com>
2026-05-29 18:27:59 +0330

    feat(viewmodel): add paged coins flow with cachedIn

--- app/src/main/java/ir/kasebvatan/crypto/presentation/viewmodel/CoinListViewModel.kt
+++ app/src/main/java/ir/kasebvatan/crypto/presentation/viewmodel/CoinListViewModel.kt
@@ -2,9 +2,11 @@ package ir.kasebvatan.crypto.presentation.viewmodel
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import androidx.paging.cachedIn
 import dagger.hilt.android.lifecycle.HiltViewModel
 import ir.kasebvatan.crypto.domain.model.Coin
 import ir.kasebvatan.crypto.domain.model.Resource
+import ir.kasebvatan.crypto.domain.usecase.GetCoinsPagedUseCase
 import ir.kasebvatan.crypto.domain.usecase.GetCoinsUseCase
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
@@ -15,10 +17,16 @@ import javax.inject.Inject
 
 @HiltViewModel
 class CoinListViewModel @Inject constructor(
-    private val getCoinsUseCase: GetCoinsUseCase
+    private val getCoinsUseCase: GetCoinsUseCase,
+    private val getCoinsPagedUseCase: GetCoinsPagedUseCase
 ) : ViewModel() {
     private val _state = MutableStateFlow<Resource<List<Coin>>>(Resource.Loading())
     val state: StateFlow<Resource<List<Coin>>> = _state
+
+
+    val coinsPaged = getCoinsPagedUseCase()
+        .cachedIn(viewModelScope)
+
 
     init {
         getCoins()
