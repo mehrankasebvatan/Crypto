7340323729e11650c7354127265e8ce9e8d14e0a
MehranKasebvatan <mehran.kasebvatan@gmail.com>
2026-05-29 11:31:16 +0330

    feat(viewmodel): add CoinListViewModel with StateFlow and Hilt injection

--- app/src/main/java/ir/kasebvatan/crypto/presentation/viewmodel/CoinListViewModel.kt
+++ app/src/main/java/ir/kasebvatan/crypto/presentation/viewmodel/CoinListViewModel.kt
@@ -2,6 +2,7 @@ package ir.kasebvatan.crypto.presentation.viewmodel
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
 import ir.kasebvatan.crypto.domain.model.Coin
 import ir.kasebvatan.crypto.domain.model.Resource
 import ir.kasebvatan.crypto.domain.usecase.GetCoinsUseCase
@@ -11,6 +12,8 @@ import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
 import javax.inject.Inject
 
+
+@HiltViewModel
 class CoinListViewModel @Inject constructor(
     private val getCoinsUseCase: GetCoinsUseCase
 ) : ViewModel() {
